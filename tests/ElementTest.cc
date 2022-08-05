612163dad06fa91ebc69b7a913e07f6bf1f7c8c3
Flocke <doerte.pieper@rwth-aachen.de>
2022-08-05 16:32:00 +0200

    Add element.variables_passive(..)

--- tests/ElementTest.cc
+++ tests/ElementTest.cc
@@ -23,3 +23,31 @@ TEST(ElementTest, ElementTest)
     // auto value. Should NOT compile
 //    vf.add_elements<1, 2>(TinyAD::range(1), [] (auto element) { return Eigen::Vector2d(0.0, 0.0); });
 }
+
+TEST(ElementTest, VariablesPassive)
+{
+    auto sf = TinyAD::scalar_function<1>(TinyAD::range(2));
+    sf.add_elements<1>(TinyAD::range(1), [] (auto& element)
+    {
+        using T = TINYAD_SCALAR_TYPE(element);
+        Eigen::Vector<T,1> a = element.variables_passive(1);
+        Eigen::Vector<T,1> b = element.variables_passive(1);
+        Eigen::Vector<T,1> c = element.variables_passive(1);
+        return element.variable(0);
+    });
+    sf.eval(Eigen::Vector2d::Zero());
+    sf.eval_with_derivatives(Eigen::Vector2d::Zero());
+
+    auto vf = TinyAD::vector_function<1>(TinyAD::range(2));
+    vf.add_elements<1, 1>(TinyAD::range(1), [] (auto& element)
+    {
+        using T = TINYAD_SCALAR_TYPE(element);
+        Eigen::Vector<T,1> a = element.variables_passive(1);
+        Eigen::Vector<T,1> b = element.variables_passive(1);
+        Eigen::Vector<T,1> c = element.variables_passive(1);
+        return Eigen::Vector<T,1> (element.variable(0));
+    });
+    vf.eval(Eigen::Vector2d::Zero());
+    vf.eval_with_jacobian(Eigen::Vector2d::Zero());
+
+}
