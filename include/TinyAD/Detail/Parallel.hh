b63c9fd28a8170663cf97a660d4ec4f673f0934c
Patrick Schmidt <patrick.schmidt@cs.rwth-aachen.de>
2024-10-27 23:51:53 +0100

    Prevent crash on exception in omp thread: Now catch and re-throw in main thread

---
+++ include/TinyAD/Detail/Parallel.hh
@@ -0,0 +1,71 @@
+/*
+ * This file is part of TinyAD and released under the MIT license.
+ * Author: Patrick Schmidt
+ */
+#pragma once
+
+#include <TinyAD/Detail/EvalSettings.hh>
+#include <Eigen/Dense>
+#include <optional>
+#include <atomic>
+#include <mutex>
+
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+namespace TinyAD
+{
+
+inline int get_n_threads(
+        const EvalSettings& _settings)
+{
+    #ifdef _OPENMP
+        if (_settings.n_threads >= 1)
+            return _settings.n_threads;
+        else
+            return std::max(1, omp_get_max_threads() - 1); // Don't use all available threads to prevent random hangs.
+    #else
+        return 1;
+    #endif
+}
+
+/**
+ * Runs the specified lambda function in an omp for loop.
+ * On throw: Catches first exception and re-throws it in the main thread.
+ */
+inline void parallel_for(
+        const Eigen::Index n,
+        const EvalSettings& settings,
+        std::function<void(int)> function_body)
+{
+    std::mutex exception_mutex;
+    std::optional<std::exception_ptr> exception_ptr;
+    std::atomic<bool> cancel_requested(false);
+
+    #pragma omp parallel for schedule(static) num_threads(get_n_threads(settings))
+    for (Eigen::Index i = 0; i < n; ++i)
+    {
+        if (cancel_requested)
+            continue;
+
+        try
+        {
+            function_body(i);
+        }
+        catch (const std::exception& ex)
+        {
+            std::lock_guard<std::mutex> lock(exception_mutex);
+            if (!exception_ptr.has_value())
+                exception_ptr = std::make_exception_ptr(ex);
+
+            cancel_requested = true;
+        }
+    }
+
+    // Re-throw, if we stored an exception
+    if (exception_ptr.has_value())
+        std::rethrow_exception(exception_ptr.value());
+}
+
+}
---
+++ tests/ExceptionTest.cc
@@ -0,0 +1,51 @@
+/*
+ * This file is part of TinyAD and released under the MIT license.
+ * Author: Patrick Schmidt
+ */
+#include <gtest/gtest.h>
+#include <TinyAD/ScalarFunction.hh>
+
+TEST(ExceptionTest, ExceptionTest)
+{
+    const int n_elements = 10;
+    bool throw_exception = true;
+
+    using Vector1 = Eigen::Matrix<double, 1, 1>;
+    auto func = TinyAD::scalar_function<1>(TinyAD::range(1));
+    func.template add_elements<1>(TinyAD::range(n_elements), [&] (auto& element) -> TINYAD_SCALAR_TYPE(element)
+    {
+        if (throw_exception)
+            throw std::runtime_error("Exception in parallel section");
+        else
+            return 1.0;
+    });
+
+    bool caught_first_exception = false;
+    bool caught_second_exception = false;
+
+    try
+    {
+        auto [f, g, H] = func.eval_with_hessian_proj(Vector1(0.0));
+    }
+    catch (const std::exception& ex)
+    {
+        caught_first_exception = true;
+    }
+
+    try
+    {
+        auto [f, g, H] = func.eval_with_hessian_proj(Vector1(0.0));
+    }
+    catch (const std::exception& ex)
+    {
+        caught_second_exception = true;
+    }
+
+    TINYAD_ASSERT(caught_first_exception);
+    TINYAD_ASSERT(caught_second_exception);
+
+    // Make sure we can still proceed normally
+    throw_exception = false;
+    auto [f, g, H] = func.eval_with_hessian_proj(Vector1(0.0));
+    TINYAD_ASSERT_EPS(f, n_elements, 1e-12);
+}
